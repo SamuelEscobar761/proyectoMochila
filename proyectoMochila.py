63f3e52d1c5453e005c93e7af7a823e68816bd0f
SamuelEscobar761 <samueb8@gmail.com>
2021-05-15 18:30:04 -0400

    Ultimos detalles

--- proyectoMochila.py
+++ proyectoMochila.py
@@ -1,5 +1,5 @@
 """Una lista que guarda las ganancias maximas para cada presupuesto hasta el presupuiesto dado"""
-cantObjetos = []
+miniMochilas = []
 """una lista donde se guardan diccionarios para poder tener las cantidades usadas para cada producto"""
 cantDiferentesPesos = []
 """limite de stock del producto"""
@@ -9,118 +9,108 @@ productos = {}
 """El costo de produccion para cada producto"""
 costProd =  []
 """ganancia por producto, se quiere tener la mayor ganancia sin pasar el presupuesto"""
-ganancias = []
+gananciasNetas = []
 
 def contarGananciaLimite(PresupuestoActual):
     for i in range(len(costProd)):
         pesoActual = costProd[i]
-        valorActual = ganancias[i]
+        valorActual = gananciasNetas[i]
         for x in range(cantidadMaxima[i]):
             for j in range(PresupuestoActual, pesoActual-1, -1):
-                if cantObjetos[j-pesoActual] + valorActual > cantObjetos[j]:
+                if miniMochilas[j-pesoActual] + valorActual > miniMochilas[j]:
                     for k in range(len(cantDiferentesPesos[j])):
                         cantDiferentesPesos[j][costProd[k]] = cantDiferentesPesos[j-pesoActual][costProd[k]]
-                    cantDiferentesPesos[j][pesoActual] = cantDiferentesPesos[j][pesoActual]+1                    
-                    cantObjetos[j] = cantObjetos[j-pesoActual] + valorActual
-                
+                    cantDiferentesPesos[j][pesoActual] = cantDiferentesPesos[j][pesoActual]+1
+                    miniMochilas[j] = miniMochilas[j-pesoActual] + valorActual
+
 def ChoiceExample(NroEjemplo):
     global cantidadMaxima
     global productos
     global costProd
-    global ganancias
-    global cantObjetos
+    global gananciasNetas
+    global miniMochilas
     global cantDiferentesPesos
     cantDiferentesPesos.clear()
-    cantObjetos.clear()
+    miniMochilas.clear()
     cantidadMaxima.clear()
     productos.clear()
     costProd.clear()
-    ganancias.clear()
+    gananciasNetas.clear()
     if(NroEjemplo == 1):
+        print("Ejecutando ejemplo de autos...")
         cantidadMaxima = [2, 3, 2, 4, 3, 2]
         productos = {6592: "Suzuki Swift", 1360: "Suzuki Santana", 16000: "Nissan Patrol 2011", 200000: "Lamborghini URUS", 39580: "BMW X7", 15960: "Tesla Cybertruck RWD"}
         costProd = [6592, 1360, 16000, 200000, 39580, 15960]
-        ganancias = [9888, 2040, 24000, 300000, 59370, 23940]
+        gananciasNetas = [9888, 2040, 24000, 300000, 59370, 23940]
         Presupuesto = 300000
         Mochila(Presupuesto)
 
     elif(NroEjemplo==2):
+        print("Ejecutando ejemplo de zapatos...")
         cantidadMaxima = [7, 8, 5, 6, 8, 3]
         productos = {94: "Adidas Nemeziz 19", 85: "Adidas Mutator", 93: "Adidas The Predator",
                      84: "Nike Mercurial Superfly 8 Elite FG", 72: "Nike Mercurial Vapor 14 Elite FG",
                      81: "Nike Phantom GT Elite Dynamic Fit 3D FG"}
         costProd = [94, 85, 93, 84, 72, 81]
-        ganancias = [40, 36, 40, 36, 31, 35]
+        gananciasNetas = [40, 36, 40, 36, 31, 35]
         Presupuesto = 1000
         Mochila(Presupuesto)
 
     elif (NroEjemplo == 3):
+        print("Ejecutando ejemplo de mouses...")
         cantidadMaxima = [8, 10, 15, 5, 5, 20]
         productos = {40: "Logitech G 910", 14: "Razer Gaming Viper", 6: "Pictek Mouse",
                      21: "Razer Basilisk X", 70: "Logitech G PRO X Superlight Inalambrico",
                      4: "VEGCOO C10"}
         costProd = [40, 14, 6, 21, 70, 4]
-        ganancias = [55, 16, 8, 24, 85, 6]
+        gananciasNetas = [55, 16, 8, 24, 85, 6]
         Presupuesto = 500
         Mochila(Presupuesto)
 
     elif (NroEjemplo == 4):
+        print("Ejecutando ejemplo de procesadores...")
         cantidadMaxima = [10, 18, 32, 24, 38, 22]
         productos = {126: "AMD Ryzen 7 5800X", 210: "AMD Ryzen 9 5900X", 66: "Intel Core i5-9600K",
                      96: "Intel Core i7-10700K", 57: "AMD Ryzen 5 2600",
                      114: "Intel Core i9-9900K"}
         costProd = [126, 210, 66, 96, 57, 114]
-        ganancias = [294, 490, 154, 224, 133, 266]
+        gananciasNetas = [294, 490, 154, 224, 133, 266]
         Presupuesto = 2000
         Mochila(Presupuesto)
 
 
     elif (NroEjemplo == 5):
-
+        print("Ejecutando ejemplo de panetones...")
         cantidadMaxima = [20, 20, 46, 23, 78, 46, 12]
-
         productos = {30: "Panetón Gustossi", 40: "Panetón La Francesa ChocoChip", 80: "Panetón D'onfrio Chocotón",
-
                      120: "Panetón Anapqui Real", 20: "Panetón Todinno",
-
                      42: "Panetón San Gabriel", 35: "Panetón San Gabriel Deli-Choc"}
-
         costProd = [30, 40, 80, 120, 20, 42, 35]
-
-        ganancias = [35, 48, 90, 140, 23, 50, 46]
-
+        gananciasNetas = [5, 8, 10, 20, 3, 8, 11]
         Presupuesto = 2500
-
         Mochila(Presupuesto)
 
 
     elif (NroEjemplo == 6):
-
+        print("Ejecutando ejemplo de consolas...")
         cantidadMaxima = [10, 16, 30, 32, 20, 25, 14, 20]
-
         productos = {450: "PlayStation 5", 355: "PlayStation 5 Online Edition", 480: "XBOX Series X",
-
                      280: "XBOX Series S", 245: "Nintendo Switch",
-
                      200: "Nintendo Switch Lite", 380: "PlayStation 4 Pro", 400: "XBOX One X"}
-
         costProd = [450, 355, 480, 280, 245, 200, 380, 400]
-
-        ganancias = [500, 400, 500, 300, 285, 220, 410, 440]
-
+        gananciasNetas = [50, 45, 20, 20, 40, 20, 30, 40]
         Presupuesto = 21000
-
         Mochila(Presupuesto)
-    
+
     elif(NroEjemplo == 0):
-        cant = input("Introduce el numero de productos que tiene: ")        
+        cant = input("Introduce el numero de productos que tiene: ")
         while not cant.isdigit() or not (int(cant) > 0):
             cant = input(f"La entrada '{cant}' no es valida, intenta otra vez: ")
         cant = int(cant)
 
         for i in range(cant):
             temp = input(f"Introduce el nombre del producto {i+1}: ")
-            temp2 = input(f"Introduce la cantidad maxima de '{temp}': ")
+            temp2 = input(f"Introduce la cantidad maxima de '{temp}' que no sea igual a uno anterior: ")
             while not temp2.isdigit() or not (int(temp2) > 0):
                 temp2 = input(f"La entrada '{temp2}' no es valida, intenta otra vez: ")
             temp2 = int(temp2)
@@ -132,47 +122,54 @@ def ChoiceExample(NroEjemplo):
             temp3 = int(temp3)
             costProd.append(temp3)
 
-            temp4 = input(f"Introduce la ganancia generada del producto '{temp}': ")
+            temp4 = input(f"Introduce la ganancia neta del producto '{temp}': ")
             while not temp4.isdigit() or not (int(temp4) > 0):
                 temp4 = input(f"La entrada '{temp4}' no es valida, intenta otra vez: ")
             temp4 = int(temp4)
-            ganancias.append(temp4)
+            gananciasNetas.append(temp4)
             productos[temp3] = temp
-        
+
         Presupuesto = input("Introduce el presupuesto que tiene: ")
         while not Presupuesto.isdigit() or not (int(Presupuesto) > 0):
                 Presupuesto = input(f"La entrada '{Presupuesto}' no es valida, intenta otra vez: ")
         Presupuesto = int(Presupuesto)
-        print()
+        print("Ejecuntando ejemplo propio...")
         Mochila(Presupuesto)
-            
+        print("============================================================================================================================================================================")
 
-    
+
+
 
 
 
 def Mochila(Presupuesto):
     for i in range(Presupuesto+1):
-        cantObjetos.append(0)
+        miniMochilas.append(0)
         cantidades = {}
         for j in costProd:
             cantidades[j] = 0
         cantDiferentesPesos.append(cantidades)
 
-    contarGananciaLimite(Presupuesto)    
-    print(f"Para presupuesto de {Presupuesto} se tiene:")
-    print("Ganancia Maxima", cantObjetos[Presupuesto])
+    contarGananciaLimite(Presupuesto)
+
+    print("Ejecutado con éxito")
+    print("============================================================================================================================================================================")
+    print(f"Para un presupuesto de {Presupuesto} se tiene:")
+    inversion = 0
+    for i in cantDiferentesPesos[Presupuesto]:
+        inversion += cantDiferentesPesos[Presupuesto][i]*i
+
+    print(f"Con una inversion de {inversion} se consigue un total de {miniMochilas[Presupuesto]+inversion} donde se tiene una ganancia maxima neta de {miniMochilas[Presupuesto]}.")
 
     for i in cantDiferentesPesos[Presupuesto]:
         if cantDiferentesPesos[Presupuesto][i] != 0:
-            print(f"Producir {cantDiferentesPesos[Presupuesto][i]} de {productos[i]} con coste de {cantDiferentesPesos[Presupuesto][i]*i}")
-    print("============================================================================================================================================================================") 
+            print(f"Producir {cantDiferentesPesos[Presupuesto][i]} de '{productos[i]}' con coste de {cantDiferentesPesos[Presupuesto][i]*i}.")
+    print("============================================================================================================================================================================")
     Choice2 = ""
     while(Choice2 != "MENU"):
         Choice2 = input("Escriba 'MENU' para volver al menu: " )
-    print()
 
-    
+
 def Menu():
     print("============================================================================================================================================================================")
     print("Escribe 'E1' para ir al ejemplo 1.")
@@ -182,13 +179,13 @@ def Menu():
     print("Escribe 'E5' para ir al ejemplo 5.")
     print("Escribe 'E6' para ir al ejemplo 6.")
     print("Escribe 'E' para generar un ejemplo nuevo.")
-    print("Escribe 'EXIT' para salir del programa.")
+    print("Escribe 'FIN' para salir del programa.")
     print("============================================================================================================================================================================")
 
 class main():
     Menu()
     Choice = input()
-    while(Choice != "EXIT"):
+    while(Choice != "FIN"):
         if(Choice == "E1"):
             ChoiceExample(1)
         elif(Choice == "E2"):
